37cdb0b184097f1e20f5126a3579e88422ab2dbd
darklight1236 <gamasutora@yandex.ru>
2019-11-28 16:54:28 +0400

    test ts

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-//nothing
+let stpbl1 = document.getElementById("stepbl1");
+let stpblrd1 = document.getElementById("stepbrdr1");
+
+stepbl1.onmouseover = function() {
+	stpblrd1.style.borderBottom = "3px solid white";	
+}
+stepbl1.onmouseout = function() {
+	stpblrd1.style.borderBottom = "3px solid black";	
+}
